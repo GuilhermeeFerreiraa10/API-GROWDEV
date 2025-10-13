d46520c4dc2bc4c6599c1bbe89fa8b703e9e9ab0
Guilherme Ferreira <guilhermefreichert@gmail.com>
2025-10-13 08:56:48 -0300

    Atualizando

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 //Criar ROTAS
 
-app.listen(3000, () => {
-    console.log("Servidor está Aberto!");
+const porta = process.env.PORT;
+
+app.listen(porta, () => {
+    console.log("Servidor está Aberto!" + porta);
     
 });
