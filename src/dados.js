8716b1266bbc3ed3f27fa99dc2e6e692b4f35357
Guilherme Ferreira <guilhermefreichert@gmail.com>
2025-10-14 16:28:01 -0300

    Atualizando

--- src/dados.js
+++ src/dados.js
@@ -1,20 +1,18 @@
+import { randomUUID } from "crypto";
+
 export const growdevers = [
-{
-    id: 1,
+    {
+    id: randomUUID(),
     nome: "Guilherme",
     email: "gui@example.com",
     idade: "19",
-    resumo: "Sou um estudade de programação",
-    skills: ["JS", "HTML", "CSS", "..."],
     matriculado: true
 },
 {
-    id: 2,
+    id: randomUUID(),
     nome: "Rene",
     email: "rene@example.com",
     idade: "49",
-    resumo: "Sou chefe na area industrial de saptos",
-    skills: ["AutoCAD", "SENAI", "..."],
     matriculado: false  
 }
 ];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { growdevers } from './dados.js';
+import { randomUUID } from 'crypto';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
 //CRIANDO ROTAS
 // GET /growdevers - Listar growdevers
-app.get("/growdevers", (req, res) => {
+app.get("/growdevers", (req,res) => {
     res.status(200).send({
         ok: true,
         mensagem: "Growdevers testados com sucesso",
@@ -17,8 +17,30 @@ app.get("/growdevers", (req, res) => {
     });
 });
 
+app.post("/growdevers", (req,res) => {
+    //1 - começo
+    const body = req.body;  
+    const novoGrowdever = {
+        id: randomUUID(),
+        nome: body.nome,
+        email: body.email,
+        idade: body.idade,
+        matriculado: body.matriculado
+    }
+
+    //2 - meio/processamento
+    growdevers.push(novoGrowdever);
+    
+    //3 - final
+    res.status(201).send({
+        ok: true,
+        mensage: "Growdever criado com sucesso",
+        dados: growdevers
+    });
+});
+
+
 const porta = process.env.PORT;
 app.listen(porta, () => {
     console.log("Servidor está Aberto!" + porta);
-    
 });
